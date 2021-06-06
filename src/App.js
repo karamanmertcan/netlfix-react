14013d1cfa2b046d805acf6f41ca57ed70e05962
mertcan <mertcankaraman2000@gmail.com>
2021-06-06 21:37:15 +0300

    1

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <AuthProvider>
       <MovieState>
-        <Router basename='/home'>
+        <Router basename='/'>
           <Fragment>
             <Switch>
               <Route exact path='/' component={Home} />
